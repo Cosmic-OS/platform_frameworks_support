cbad15f042c4606fb023f59ae893a343dbb03510
Sagar Shah <sagarshah2326@gmail.com>
2018-10-26 15:50:48 +0000

    Revert "Factor out preference-in-storage check to overridable method."
    This reverts commit 20d07fe4eae7fcf82b838f445faee777a3effb58.

--- preference/src/main/java/androidx/preference/Preference.java
+++ preference/src/main/java/androidx/preference/Preference.java
@@ -1510,14 +1510,6 @@ public class Preference implements Comparable<Preference> {
         mDefaultValue = defaultValue;
     }
 
-     /**
-     * Returns whether the preference can be found in persistent storage
-     * @hide
-     */
-    protected boolean isPersisted() {
-        return getSharedPreferences().contains(mKey);
-    }
-
     private void dispatchSetInitialValue() {
         if (getPreferenceDataStore() != null) {
             onSetInitialValue(true, mDefaultValue);
@@ -1526,7 +1518,7 @@ public class Preference implements Comparable<Preference> {
 
         // By now, we know if we are persistent.
         final boolean shouldPersist = shouldPersist();
-        if (!shouldPersist || !isPersisted()) {
+        if (!shouldPersist || !getSharedPreferences().contains(mKey)) {
             if (mDefaultValue != null) {
                 onSetInitialValue(false, mDefaultValue);
             }
